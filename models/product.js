6226abedaba4c8af4ceb343a25fb5cb7f84c546d
Samuel Ribeiro de Freitas <samuelr@take.net>
2020-03-10 12:52:18 -0300

    changed Product model logic

--- models/product.js
+++ models/product.js
@@ -1,6 +1,21 @@
 const products = []
 const fs = require('fs')
 const path = require('path')
+const p = path.join(
+    path.dirname(
+        process.mainModule.filename
+    ),'data','products.json'
+)
+
+const getProductsFromFile = (cb) => {
+    
+    fs.readFile(p, (err,fileContent) => {
+        if(err){
+            return cb([])
+        }
+        cb(JSON.parse(fileContent))
+    })
+}
 
 class Product{
 
@@ -13,7 +28,7 @@ class Product{
     }
 
     static fetchAll(){
-        return products
+        return getProductsFromFile(cb)
     }
 
 }
@@ -23,33 +38,13 @@ module.exports = function Product(title){
     this.title = title
 
     this.fetchAll = function(cb){
-        const p = path.join(
-            path.dirname(
-                process.mainModule.filename
-            ),'data','products.json'
-        )
-        fs.readFile(p, (err,fileContent) => {
-            if(err){
-                return cb([])
-            }
-            return cb(JSON.parse(fileContent))
-        })
+        return getProductsFromFile(cb)
     }
 
     this.save = function(){
-        const p = path.join(
-            path.dirname(
-                process.mainModule.filename
-            ),'data','products.json'
-        )
-        products.push(this)
-        fs.readFile(p,(err,fileContent) => {
-            let products = []
-            if(!err){
-                products = JSON.parse(fileContent)
-            }
+        getProductsFromFile(products => {
             products.push(this)
-            fs.writeFile(p,JSON.stringify(products), (err) => {
+            fs.writeFile(p,JSON.stringify(products), err => {
                 console.log(err)
             })
         })
