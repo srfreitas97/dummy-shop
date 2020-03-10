19094eddbea7263e5619cc47d67fc19ab8b2d319
Samuel Ribeiro de Freitas <samuelr@take.net>
2020-03-10 12:09:17 -0300

    Added static file for products

--- models/product.js
+++ models/product.js
@@ -1,4 +1,6 @@
 const products = []
+const fs = require('fs')
+const path = require('path')
 
 class Product{
 
@@ -21,11 +23,37 @@ module.exports = function Product(title){
     this.title = title
 
     this.fetchAll = function(){
+        const p = path.join(
+            path.dirname(
+                process.mainModule.filename
+            ),'data','products.json'
+        )
+        fs.readFile(p, (err,fileContent) => {
+            if(err){
+                return []
+            }
+            return JSON.parse(fileContent)
+        })
         return products
     }
 
     this.save = function(){
+        const p = path.join(
+            path.dirname(
+                process.mainModule.filename
+            ),'data','products.json'
+        )
         products.push(this)
+        fs.readFile(p,(err,fileContent) => {
+            let products = []
+            if(!err){
+                products = JSON.parse(fileContent)
+            }
+            products.push(this)
+            fs.writeFile(p,JSON.stringify(products), (err) => {
+                console.log(err)
+            })
+        })
     }
 
 
